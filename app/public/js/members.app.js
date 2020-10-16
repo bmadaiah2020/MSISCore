c635120a64e55d35384d562a62e43e7932363f28
Binita Madaiah <bmadaiah@indiana.edu>
2020-10-16 00:05:12 -0400

    Added the members.ap.js and report.app.js

---
+++ app/public/js/members.app.js
@@ -0,0 +1,34 @@
+waitingApp = new Vue({
+  el: "#PersonalID",
+  data:{
+    persons:[{
+      firstName:'',
+      lastName:'',
+      street:'',
+      city:'',
+      state:'',
+      zipcode:'',
+      radioNumber:'',
+      stationNumber:'',
+      gender:'',
+      email:'',
+      position:'',
+      isActive:'',
+      dateofBirth:''
+  }]
+},
+methods:{
+  methods:{
+    fetchUser(api/person/')
+    .then(response => response.json())
+    .then(json =>{
+      this.persons=json;
+      console.log(this.persons);
+    });
+  }
+
+},
+created(){
+  this.fetchUser();
+}
+});
