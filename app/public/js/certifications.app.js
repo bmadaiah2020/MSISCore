cfd8fad0e099abf24573127aa51dcd551c15cc44
awvandyke <39847070+awvandyke@users.noreply.github.com>
2020-10-28 17:22:32 -0400

    certifications js

---
+++ app/public/js/certifications.app.js
@@ -0,0 +1,51 @@
+var app = new Vue({
+  el: "#CertificationPage",
+  data:{
+    certificationList: [],
+    newcertificationForm: {}
+  },
+
+
+  methods: {
+
+    newCertificationData() {
+      return {
+        PersonalID: '',
+        CertificationID: '',
+        expirationDate: '',
+        certificationStatus: '',
+        renewedDate: ''
+      }
+    },
+
+    handleNewCertification(evt) {
+      fetch("api/certification/add.php", {
+        method: 'POST',
+        body: JSON.stringify(this.newCommentForm),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+          }
+        })
+      .then(response => response.json())
+      .then(json =>  {
+        console.log("Returned from post:", json);
+        this.certificationList = json;
+        this.certificationForm = this.newCertificationData();
+      });
+      console.log("Creating (POSTing)...!");
+      console.log(this.newcertificationForm);
+    },
+    },
+
+    created() {
+      fetch("api/certification/")
+      .then(response => response.json())
+      .then(json => {
+        this.certificationList = json;
+
+        console.log(json);
+        console.log("created");
+      });
+      this.newcertificationForm = this.newCertificationData();
+    }
+  });
