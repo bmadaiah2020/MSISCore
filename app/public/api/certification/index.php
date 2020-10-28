44ddd8ac9b500a9325bea2f295f263dd13aa3665
awvandyke <39847070+awvandyke@users.noreply.github.com>
2020-10-28 17:06:57 -0400

    certification index.php

--- app/public/api/certification/index.php
+++ app/public/api/certification/index.php
@@ -6,22 +6,22 @@ require 'common.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM Person';
+$sql = 'SELECT * FROM Person_Certification';
 $vars = [];
 
 if (isset($_GET['PersonalID'])) {
   // This is an example of a parameterized query
-  $sql = 'SELECT * FROM Person WHERE PersonalID = ?';
+  $sql = 'SELECT * FROM Person_Certification WHERE PersonalID = ?';
   $vars = [ $_GET['PersonalID'] ];
 }
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$person = $stmt->fetchAll();
+$certification = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($person, JSON_PRETTY_PRINT);
+$json = json_encode($certification, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
