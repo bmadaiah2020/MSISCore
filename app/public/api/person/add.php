04efe0a52276ea805c202667639ff08ea211f0af
Binita Madaiah <bmadaiah@indiana.edu>
2020-10-28 18:54:53 -0400

    Post stuff for memebers table

--- app/public/api/person/add.php
+++ app/public/api/person/add.php
@@ -5,25 +5,29 @@ require 'common.php';
 $db = DBConnection::getConnection();
 
 $stmt = $db->prepare(
-  *INSERT INTO People (firstName, lastName, position, radioNumber, stationNumber, street, city, state, zipcode, gender, isActive, dateofBirth, email)
-  Values (?, ?, ?, ?, ?, ?)*
+  'INSERT INTO People (firstName, lastName, street, city, state, zipcode, radioNumber, stationNumber, gender, email, position, isActive, dateofBirth)
+  Values (?, ?, ?, ?, ?, ?)'
 );
 
 $stmt->execute([
   $_POST['firstName'],
   $_POST['lastName'],
-  $_POST['position'],
-  $_POST['radioNumber'],
-  $_POST['stationNumber'],
   $_POST['street'],
   $_POST['city'],
   $_POST['state'],
   $_POST['zipcode'],
+  $_POST['radioNumber'],
+  $_POST['stationNumber'],
   $_POST['gender'],
-  $_POST['isActive'],
-  $_POST['dateofBirth'],
   $_POST['email'],
+  $_POST['position'],
+  $_POST['isActive'],
+  $_POST['dateofBirth']
 ]);
 
+$pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
+
+
 header('HTTP/1.1 303 See Other');
-header('Location: ../records/?guid=' . $guid);
+header('Content-Type: application/json');
+header('Location: ../person/?id=' . $pk);
--- app/public/js/members.app.js
+++ app/public/js/members.app.js
@@ -15,8 +15,27 @@ var App = new Vue({
       position:'',
       isActive:'',
       dateofBirth:''
-  }]
-},
+  }],
+  newPerson:{
+    firstName:'',
+    lastName:'',
+    street:'',
+    city:'',
+    state:'',
+    zipcode:'',
+    radioNumber:'',
+    stationNumber:'',
+    gender:'',
+    email:'',
+    position:'',
+    isActive:'',
+    dateofBirth:''
+  }
+
+  created(){
+    this.fetchUser();
+  },
+
 methods:{
     fetchUser() {
       fetch("api/person/")
@@ -25,10 +44,44 @@ methods:{
       this.persons=json;
       console.log(this.persons);
     });
-  }
+
+  },
+  addUser() {
+     fetch('api/person/add.php', {
+       method:'POST',
+       body: JSON.stringify(this.newUser),
+       headers: {
+         "Content-Type": "application/json; charset=utf-8"
+       }
+     })
+     .then( response => response.json())
+     .then( json => {
+       console.log("Returned from post:", json);
+       // TODO: test a result was returned!
+       this.comments = json;
+       this.newComment = this.newUserData();
+     });
+     console.log("Creating (POSTing)...!");
+     console.log(this.newUser);
+   },
+newCommentData() {
+  return{
+    firstName:'',
+    lastName:'',
+    street:'',
+    city:'',
+    state:'',
+    zipcode:'',
+    radioNumber:'',
+    stationNumber:'',
+    gender:'',
+    email:'',
+    position:'',
+    isActive:'',
+    dateofBirth:''
+}}
 
 },
-created(){
-  this.fetchUser();
-}
-});
+
+
+})
