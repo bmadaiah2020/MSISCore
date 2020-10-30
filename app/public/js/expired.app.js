4ec91e20111dcda954d43d685c53ad5230c32e71
sabublic <70554903+sabublic@users.noreply.github.com>
2020-10-30 18:13:06 -0400

    Update expired.app.js
    troubleshooting

--- app/public/js/expired.app.js
+++ app/public/js/expired.app.js
@@ -18,50 +18,23 @@ var App = new Vue({
   },
   created(){
     this.fetchUser();
+    console.log("fetched");
   },
 
   methods:{
       fetchUser() {
-        fetch("api/reports/")
+        fetch("api/reports/index.php")
       .then(response => response.json())
       .then(json =>{
         this.exreports=json;
         console.log(this.exreports);
       });
-
+    }
 
   },
-  addUser() {
-    console.log('addUser() was called!'+this.newCert.PersonalID);
-     fetch('api/reports/add.php', {
-       method:'POST',
-       body: JSON.stringify(this.newCert),
-       headers: {
-         "Content-Type": "application/json; charset=utf-8"
-       }
-     })
-     .then( response => response.json())
-     .then( json => {
-       console.log("Returned from post:", json);
-       // TODO: test a result was returned!
-       this.exreports.push(json[0]);
-       this.newCert = this.newCertData();
-     });
-     console.log("Creating (POSTing)...!");
-     console.log(this.newCert);
-   },
-newCertData() {
-  return{
-    PersonalID: '',
-    CertificationID: '',
-    expirationDate: '',
-    renewedDate: ''
-}
 
-},
   formatDate(d){
     return moment.utc(d).local().calendar();
   }
-}
 
 })
