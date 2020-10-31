a9c67bc62fd56c37b0c7969e7a524ef94ad4d931
sabublic <70554903+sabublic@users.noreply.github.com>
2020-10-31 16:08:32 -0400

    update to report.html
    update to making tables display when selected

--- app/public/js/filter.js
+++ app/public/js/filter.js
@@ -1,15 +1,15 @@
 function launch(evt, cityName) {
 
-  var i, tabcontent, tablinks;
+  var i, displaytable, repview;
 
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i=0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = 'none';
+  tabcontent = document.getElementsByClassName("displaytable");
+  for (i=0; i < displaytable.length; i++) {
+    displaytable[i].style.display = 'none';
   }
 
-  tablinks = document.getElementsByClassName('tablinks');
-  for (i=0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active",'');
+  repview = document.getElementsByClassName('repview');
+  for (i=0; i < tablinks.repview; i++) {
+    repview[i].className = repview[i].className.replace(" active",'');
   }
 
   document.getElementsById(cityName).style.display = 'block';
